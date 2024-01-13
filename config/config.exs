52b14a80d059cb8572872a7a4c78777bcea36038
John Herbener <herbener.dev@gmail.com>
2024-01-13 11:11:49 -0500

    change tesla adaptor

--- config/config.exs
+++ config/config.exs
@@ -83,6 +83,8 @@ config :sentry,
   included_environments: [:prod],
   environment_name: Mix.env()
 
+config :tesla, :adapter, Tesla.Adapter.Hackney
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
